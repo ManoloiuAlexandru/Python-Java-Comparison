148462971440223d241dbf83c6acf8cd5dc9b586
ManoloiuAlexandru <manalexio@yahoo.com>
2023-06-23 10:54:20 +0300

    Added Python server

---
+++ Pythonserver/AllPythonProblems.py
@@ -0,0 +1,21 @@
+from flask import Flask, json, request
+
+app = Flask(__name__)
+
+
+class Server:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+
+@app.route('/first/firstproblem', methods=['POST'])
+def checked_data():
+    result = request.json
+    print(result)
+    return result
+
+
+if __name__ == '__main__':
+    server = Server(ip="127.0.0.1", port="8080")
+    app.run(host=server.ip, port=server.port)
