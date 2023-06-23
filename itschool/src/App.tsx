be84a0476a13f51413e5396a41e8e187b8ce75c7
ManoloiuAlexandru <manalexio@yahoo.com>
2023-06-23 10:48:56 +0300

    Added React Part

---
+++ itschool/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FirstProblem from './components/FirstProblem'
+function App() {
+    return (
+        <FirstProblem/>
+  );
+}
+
+export default App;
---
+++ itschool/src/components/FirstProblem.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useState } from 'react';
+import axios from "axios";
+import logo from './logo.svg';
+const baseURL = "http://127.0.0.1:8080/first/firstproblem";
+export default function FirstProblem(this: any) {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+
+    const triggerAPI = useCallback(async () => {
+        const res = await axios.post(baseURL, {
+            "name": name,
+            "age": age
+        });
+    }, [name, age]);
+    const PrintName = useCallback((e: any) => {
+        e.preventDefault()
+        triggerAPI();
+    }, [triggerAPI])
+
+    const handleChangeName = useCallback((event: any) => {
+        setName(event.target.value);
+    }, []);
+    const handleChangeAge = useCallback((event: any) => {
+        setAge(event.target.value);
+    }, []);
+    return (
+        <div>
+            <div>
+                <form onSubmit={PrintName}>
+                    <label>
+                        Name:
+                        <input type="text" value={name} name="Name" onChange={handleChangeName} />
+                    </label>
+                    <label>
+                        Age:
+                        <input type="text" value={age} name="armorType" onChange={handleChangeAge} />
+                    </label>
+                    <input type="submit" value="Print Result" />
+                </form>
+            </div>
+        </div>
+    );
+}
