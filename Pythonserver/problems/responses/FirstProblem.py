79f6c861c743f822f8b8937c056adea08edd7f8a
ManoloiuAlexandru <manalexio@yahoo.com>
2023-06-29 11:03:57 +0300

    Added doc

--- Pythonserver/problems/responses/FirstProblem.py
+++ Pythonserver/problems/responses/FirstProblem.py
@@ -1,4 +1,15 @@
+"""
+Given a name and the age of a person show on screen: 'name' has age:'age'.
+"""
+
+
 def first_problem_solution(name, age):
+    """
+    :param name: the name of the person
+    :param age: the age of the person
+    :return: a string containing the 'name' has age:'age'
+    or invalid data if one of the parameters is none
+    """
     if name is not None and age is not None:
         print(name, "has age:", age)
         return name + " has age:" + str(age)
--- Pythonserver/problems/responses/SecondProblem.py
+++ Pythonserver/problems/responses/SecondProblem.py
@@ -1,7 +1,17 @@
 from math import sqrt
 
+"""Calculate the area and perimeter of a right triangle that has the 2 sides given.
+Ex: side1=3 side2=4 => side3=5 
+Perimeter=12 and area=6
+"""
+
 
 def second_problem_solution(side1, side2):
+    """
+    :param side1: one side of the triangle
+    :param side2: second side of the triangle
+    :return: Perimeter and area of the triangle
+    """
     side3 = sqrt(pow(side1, 2) + pow(side2, 2))
     perimeter = side1 + side2 + side3
     area = side1 * side2 / 2
