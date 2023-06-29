f35fd87cbde14b36bc1c02b9128690e3227810ed
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-06-29 20:56:37 +0300

    Update CurrencyConvertorServices.java

--- Javaserver/ITSchool/src/main/java/demoschool/ITSchool/services/CurrencyConvertorServices.java
+++ Javaserver/ITSchool/src/main/java/demoschool/ITSchool/services/CurrencyConvertorServices.java
@@ -16,11 +16,17 @@ import com.google.gson.reflect.TypeToken;
 import demoschool.ITSchool.data.CurrencyToConvert;
 import lombok.RequiredArgsConstructor;
 
+/*
+ * @author Manoloiu Ionut-Alexandru
+ */
 @Service
 @RequiredArgsConstructor
 public class CurrencyConvertorServices {
 
 	public HashMap<String, String> getCurrencyConverting() throws IOException, InterruptedException {
+		/*Method that gets the data from the python server and converts it to a HashMap
+		 * @returns a HashMap with the vales of from the current bnr exchange rate
+		 */
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://127.0.0.1:8081/currencys")).build();
 
@@ -28,11 +34,22 @@ public class CurrencyConvertorServices {
 		Map<String, String> currencyMap = new Gson().fromJson(response.body(),
 				new TypeToken<HashMap<String, String>>() {
 				}.getType());
-	return (HashMap<String, String>) currencyMap;
+		return (HashMap<String, String>) currencyMap;
 	}
 
 	public String CurrencyConverting(CurrencyToConvert currencyToConvert) throws IOException, InterruptedException {
-		HashMap<String, String> currency_dict=this.getCurrencyConverting();
-		return currencyToConvert.getCurrency()+ " " +String.valueOf(Float.parseFloat(currency_dict.get(currencyToConvert.getCurrency()))/Float.parseFloat(currency_dict.get(currencyToConvert.getCurrencyToConvertTo()))*currencyToConvert.getAmount())+" = "+currencyToConvert.getCurrencyToConvertTo();
+		/*
+		 * Function that converts the amount you send to the amount you want the
+		 * function returns the result in string form. The result is obtained by doing
+		 * by multiplying the amount with the exchange
+		 * rate:currencyToConvert/currencyToConvertTo
+		 * @return a string representing the result
+		 */
+		HashMap<String, String> currency_dict = this.getCurrencyConverting();
+		return currencyToConvert.getCurrency() + " "
+				+ String.valueOf(Float.parseFloat(currency_dict.get(currencyToConvert.getCurrency()))
+						/ Float.parseFloat(currency_dict.get(currencyToConvert.getCurrencyToConvertTo()))
+						* currencyToConvert.getAmount())
+				+ " = " + currencyToConvert.getCurrencyToConvertTo();
 	}
 }
